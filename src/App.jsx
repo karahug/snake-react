c452ef754809105f62fcfe93c29e47e2ebbb334b
Tyrone DeSilva <tdesilva@asu.edu>
2017-02-05 09:36:36 +0000

    Setup testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
 import React from 'react';
 import autoBind from 'react-autobind';
-var KEYS = {38: 'up', 40: 'down', 37: 'left', 39: 'right'}
+var KEYS = {38: 'up', 40: 'down', 37: 'left', 39: 'right', 32: 'space'};
 var INITIAL_COORDINATES = [20,20];
+var INITIAL_SPEED = 100;
 
 class App extends React.Component{
     constructor(){
         super();
         autoBind(this);
-        this.state = {snake: [INITIAL_COORDINATES], direction: 'none', length: 1};
+        this.state = {snake: [INITIAL_COORDINATES], direction: 'none', length: 1, speed: INITIAL_SPEED};
     }
     
+    
+    componentDidMount(){
+        this._tick();
+    }
     _tick(){
         
         //callback
-        setTimeout(this._tick(), 33);
+        switch(this.direction){
+            case 'up':
+                
+            case 'down':
+                
+            case 'left':
+            
+            case 'right':
+                
+            default:
+        }
+        
+        setTimeout(this._tick(), this.speed);
     }
     _pause(){
         
     }
-    _unpause(){
-        
-    }
     _handleKey(event){
-        var direction =  event.nativeEvent.keycode;
-        this.state.direction = KEYS[direction] || this.state.direction
+        var key =  event.nativeEvent.keycode;
+        if(KEYS[key] == 'space'){
+            this._pause();
+            return;
+        }
+        this.state.direction = KEYS[key] || this.state.key;
     }
     _eat(){
         
     }
     _resume(){
-        
+        this._tick();
     }
     render(){
         return(
